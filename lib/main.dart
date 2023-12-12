cab3e4cc66104329e66bbc43f9f995166a6a5d5d
moonseunghun <mangokkaksae2@gmail.com>
2023-12-12 17:21:34 +0900

    Git action

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'bloc_observer.dart';
 import 'portfolio_app.dart';
-
+//Start
 void main() {
   Bloc.observer = MyBlocObserver();
   runApp(const PortfolioApp());
